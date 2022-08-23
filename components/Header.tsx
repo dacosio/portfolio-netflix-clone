744e77a6a718530de7c213614246d59887270171
cosio <ucg.don.cosio@gmail.com>
2022-08-23 18:27:24 +0800

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <div>
+            <img src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg" width={100} height={100} className="cursor-pointer object-contain" />
+            
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+
+        </div>
+        <div>
+
+        </div>
+    </header>
+  )
+}
+
+export default Header
